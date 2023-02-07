354c00d44f1d82bae8d1eda4547495e18969fa4a
Priyanka arora <115262200+Priyanka0899@users.noreply.github.com>
2023-02-07 19:42:16 +0530

    Update reverseArray.java

--- reverseArray.java
+++ reverseArray.java
@@ -13,6 +13,17 @@ public class ReverseArray
 				end--;
 			}
 		}
+	       static void reverse(int arr[],int start,int end)
+   		 {
+			if(start>end)
+			{
+			    return;
+			}
+			int temp=arr[start];
+			arr[start]=arr[end];
+			arr[end]=temp;
+			reverse(arr,start+1,end-1);
+   		 }
 			static void print(int arr[],int size)
 			{
 				for(int i=0;i<size;i++)
